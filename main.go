39e95eb352409f6e091d1d4c48afee5705dbd5df
Natali Zhylina <iamhiro442@gmail.com>
2020-01-02 14:21:50 +0200

    update

--- main.go
+++ main.go
@@ -8,31 +8,35 @@ func main() {
 
 	slice := []int{3, 4, 1, 2, 5, 7, -1, 0}
 	fmt.Println("\n--- Unsorted --- \n\n", slice)
-	bubblesort(slice)
+
+	order := false
+	bubblesort(slice, order)
+	fmt.Println("\n--- Sorted ---\n\n", slice)
+
+	order = true
+	bubblesort(slice, order)
+	fmt.Println("\n--- Sorted2 ---\n\n", slice, "\n")
+
 
 }
 
-func bubblesort(items []int) {
+func bubblesort(items []int, order bool) {
 	var (
 		n = len(items)
 	)
 
 	for i := 0; i < n; i++ {
 		for j := i; j < n; j++ {
-			if items[i] > items[j] {
-				items[j], items[i] = items[i], items[j]
+			if !order {
+				if items[i] > items[j] {
+					items[j], items[i] = items[i], items[j]
+				}
+			} else {
+				if items[j] > items[i] {
+					items[i], items[j] = items[j], items[i]
+				}
 			}
+
 		}
 	}
-	fmt.Println("\n--- Sorted ---\n\n", items)
-
-	for i := 0; i < n; i++ {
-		for j := i; j < n; j++ {
-			if items[j] > items[i] {
-				items[i], items[j] = items[j], items[i]
-			}
-		}
-	}
-	fmt.Println("\n--- Sorted2 ---\n\n", items, "\n")
-
 }
